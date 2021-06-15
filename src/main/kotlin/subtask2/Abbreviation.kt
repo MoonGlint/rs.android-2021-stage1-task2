ce5ffe1f242e4b6579b334a1334cdbd6fbe7e6eb
MoonGlint <GlintOfMoon@gmail.com>
2021-06-15 13:38:30 +0300

    Abbreviation added

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,18 @@
 package subtask2
-
+import java.util.regex.Pattern
 class Abbreviation {
 
-    // TODO: Complete the following function
-    fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+    fun abbreviationFromA(a: String?, b: String): String {
+        val bArray = b.split("").toTypedArray()
+        var bpattern = "\\w*"
+        for (i in bArray.indices) {
+            bpattern = bpattern + "(" + bArray[i] + "|" + bArray[i].toLowerCase() + ")" + "\\w*"
+        }
+        println(bpattern)
+        val pattern = Pattern.compile(bpattern)
+        val matcher = pattern.matcher(a)
+        return if (matcher.matches()) {
+            "YES"
+        } else "NO"
     }
 }
